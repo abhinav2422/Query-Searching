9916acaaf72186381c84c7b99eb6eec4938a3be6
Abhinav Jain <jabhi678@gmail.com>
2019-01-31 20:19:08 +0530

    string search

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const elasticsearch = require('elasticsearch');
 const rake = require('node-rake');
+const file = require('file-system');
+const fs = require('fs');
 
 const app = express();
+
+file.readFile === fs.readFile
+
+var qna = require('./qna.json')
+
+/*var client = new elasticsearch.Client({
+  host: 'localhost:5000',
+  log: 'trace'
+});
+
+client.ping({
+    requestTimeout: 2000
+    }, 
+    (error) => {
+        if (error) {
+        console.trace('elasticsearch cluster is down!');
+        } else {
+        console.log('All is well');
+    }
+});*/
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -20,9 +43,16 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/res', (req, res) => {
+    res.render('res');
+})
+
 app.post('/res', (req, res) => {
-    const keywords = rake.generate(req.body.query);
-    console.log(keywords);
+    //const keywords = rake.generate(req.body.query);
+    //console.log(keywords);
+
+    //var obj = JSON.parse(qna);
+    res.send(qna.Q1 + "<br>" + qna.A1);   
 });
 
 const port = 5000;
